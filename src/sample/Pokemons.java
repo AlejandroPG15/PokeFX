441bb538a9e3a957ec35a275f5fc3ab5a93f9e8e
AlejandroPG15 <alejandropg140600@hotmail.com>
2021-01-24 21:51:56 +0100

    Initial

---
+++ src/sample/Pokemons.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Pokemons {
+
+    String nombre;
+    int nivel;
+    int vida;
+    String imagen;
+
+    public Pokemons(String nombre, int nivel, int vida, String imagen){
+        this.nombre = nombre;
+        this.vida = nivel;
+        this.nivel = vida;
+        this.imagen = imagen;
+    }
+
+}
